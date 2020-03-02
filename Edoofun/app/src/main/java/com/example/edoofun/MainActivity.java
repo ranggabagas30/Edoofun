56c8aee0022395e188207e62c951b747e1ab9292
yuniariyaniar <ariyaniyuni467@gmail.com>
2020-03-02 18:03:35 +0700

    Membuat style buat button, dan memasukkan gambar

--- Edoofun/app/src/main/java/com/example/edoofun/MainActivity.java
+++ Edoofun/app/src/main/java/com/example/edoofun/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.edoofun;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -9,7 +10,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.beli_koin);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.ic_dehaze);
 
     }
 }
